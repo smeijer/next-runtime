c9f9a3e42cc795139bac2cbc63e46961b8c82a96
Stephan Meijer <stephan.meijer@gmail.com>
2021-10-06 21:58:15 +0200

    feat: add cookies instance to the context

--- src/runtime/cookies.ts
+++ src/runtime/cookies.ts
@@ -3,6 +3,7 @@ import Cookies from 'cookies';
 import { GetServerSidePropsContext } from '../types/next';
 
 export type CookieJar = {
+  cookies: InstanceType<typeof Cookies>;
   req: { getCookie: InstanceType<typeof Cookies>['get'] };
   res: { setCookie: InstanceType<typeof Cookies>['set'] };
 };
@@ -10,7 +11,8 @@ export type CookieJar = {
 export function bindCookieJar(
   context: GetServerSidePropsContext,
 ): asserts context is GetServerSidePropsContext & CookieJar {
-  const cookies = new Cookies(context.req, context.res);
-  (context.req as any).getCookie = cookies.get.bind(cookies);
-  (context.res as any).setCookie = cookies.set.bind(cookies);
+  const ctx = context as GetServerSidePropsContext & CookieJar;
+  ctx.cookies = new Cookies(ctx.req, ctx.res);
+  ctx.req.getCookie = ctx.cookies.get.bind(ctx.cookies);
+  ctx.res.setCookie = ctx.cookies.set.bind(ctx.cookies);
 }
