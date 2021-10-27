a67bfaa55a75604cb301cf8f9fa31a03d8148bec
Umar Ahmed <umar.ahmed1998@gmail.com>
2021-10-27 13:38:44 +0000

    cr: rename error.ts to errors.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // NOTE: do NOT export `Form` here. Next.js can't properly tree shake the client
 // bundle if there are client & server imports in a single import statement.
-export * from './error';
+export * from './errors';
 export * from './handle';
 export * from './responses';
