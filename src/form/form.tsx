b497d414106612eaff64c3e2c112085ff1a8349c
Stephan Meijer <stephan.meijer@gmail.com>
2022-05-28 16:25:15 +0200

    feat: include button name=value in submitted formdata

--- src/form/form.tsx
+++ src/form/form.tsx
@@ -1,8 +1,22 @@
 import { useRouter } from 'next/router';
-import { FormEventHandler, FormHTMLAttributes, forwardRef } from 'react';
+import {
+  BaseSyntheticEvent,
+  FormEvent,
+  FormEventHandler,
+  FormHTMLAttributes,
+  forwardRef,
+  useCallback,
+} from 'react';
 
 import { HttpMethod } from '../http-methods';
 import { useLatestRef } from '../lib/use-latest-ref';
+import {
+  getFormSubmitter,
+  isButtonElement,
+  isFormElement,
+  isInputElement,
+  isValidFormSubmitter,
+} from './dom';
 import { FormStateWithHelpers } from './helpers';
 import { useFormStoreSubmit } from './store';
 
@@ -39,7 +53,15 @@ export type FormProps<Data> = {
 >;
 
 function FormComponent<Data>(
-  { method = 'post', onSubmit, onSuccess, onError, ...props }: FormProps<Data>,
+  {
+    method,
+    action,
+    encType,
+    onSubmit,
+    onSuccess,
+    onError,
+    ...props
+  }: FormProps<Data>,
   ref: React.Ref<HTMLFormElement>,
 ) {
   const submit = useFormStoreSubmit();
@@ -47,32 +69,88 @@ function FormComponent<Data>(
   const onSuccessRef = useLatestRef(onSuccess);
   const onErrorRef = useLatestRef(onError);
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
-    onSubmit?.(event);
-    if (event.defaultPrevented) return;
+  const handleSubmit: FormEventHandler<HTMLFormElement> = useCallback(
+    async (event) => {
+      onSubmit?.(event);
+      if (event.defaultPrevented) return;
 
-    event.preventDefault();
+      event.preventDefault();
 
-    submit({
-      name: props.name,
-      router,
-      formData: new FormData(event.currentTarget),
-      formAction: props.action || location.href.split('?')[0],
+      // <button>/<input type="submit"> may override attributes of <form>
+      const submitter = getFormSubmitter(event);
+      const form = submitter ? submitter.form : event.currentTarget;
+
+      if (!form) {
+        throw new Error('Cannot submit a <button> without a <form>');
+      }
+
+      if (submitter && !isValidFormSubmitter(submitter)) {
+        throw new Error(
+          'Cannot submit element that is not <form>, <button>, or <input type="submit|image">',
+        );
+      }
+
+      const formData: FormData = new FormData(form);
+
+      const formMethod =
+        (submitter?.getAttribute('formmethod') as HttpMethod) ||
+        (form.getAttribute('method') as HttpMethod) ||
+        method ||
+        'post';
+
+      const formAction =
+        (submitter?.getAttribute('formaction') as HttpMethod) ||
+        (form.getAttribute('action') as HttpMethod) ||
+        action ||
+        location.href.split('?')[0];
+
+      // Include name + value from the submitter
+      if (submitter?.name) {
+        formData.set(submitter.name, submitter.value);
+      }
+
+      console.log('submit', formData, formAction, formMethod);
+      submit({
+        name: props.name,
+        router,
+        formData,
+        formAction,
+        method: formMethod,
+
+        // need to wrap the callbacks like this so the latest ref value is accessed
+        // at the time the callbacks are invoked, otherwise we could pass in a stale callback
+        onError: (state) => {
+          console.log('err', state);
+          onErrorRef.current?.(state as FormStateWithHelpers<Data>);
+        },
+        onSuccess: (state) => {
+          console.log('done');
+          onSuccessRef.current?.(state as FormStateWithHelpers<Data>);
+        },
+      });
+    },
+    [
       method,
+      onErrorRef,
+      onSubmit,
+      onSuccessRef,
+      action,
+      props.name,
+      router,
+      submit,
+    ],
+  );
 
-      // need to wrap the callbacks like this so the latest ref value is accessed
-      // at the time the callbacks are invoked,
-      // otherwise we could pass in a stale callback
-      onError: (state) => {
-        onErrorRef.current?.(state as FormStateWithHelpers<Data>);
-      },
-      onSuccess: (state) => {
-        onSuccessRef.current?.(state as FormStateWithHelpers<Data>);
-      },
-    });
-  };
-
-  return <form {...props} ref={ref} method={method} onSubmit={handleSubmit} />;
+  return (
+    <form
+      {...props}
+      ref={ref}
+      action={action}
+      encType={encType}
+      method={method}
+      onSubmit={handleSubmit}
+    />
+  );
 }
 
 /**
